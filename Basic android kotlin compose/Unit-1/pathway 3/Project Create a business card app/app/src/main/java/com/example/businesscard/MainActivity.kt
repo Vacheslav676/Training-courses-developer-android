94598e7d39610b6643501066e6b91a2e85ca8404
Вячеслав <115939591+Vacheslav676@users.noreply.github.com>
2025-05-20 10:00:39 +0300

    Update MainActivity.kt

--- Basic android kotlin compose/Unit-1/pathway 3/Project Create a business card app/app/src/main/java/com/example/businesscard/MainActivity.kt
+++ Basic android kotlin compose/Unit-1/pathway 3/Project Create a business card app/app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -21,7 +21,10 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dpimport
+import androidx.compose.ui.unit.dp
+import com.example.businesscard.ui.theme.BusinessCardTheme
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
